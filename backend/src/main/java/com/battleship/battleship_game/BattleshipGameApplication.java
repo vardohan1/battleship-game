2370f919c09e4df080143d0a796f13bd06551237
vakodavitashvili <vako.davitashvili@sap.com>
2025-09-16 16:49:26 +0400

    initial commit

---
+++ backend/src/main/java/com/battleship/battleship_game/BattleshipGameApplication.java
@@ -0,0 +1,13 @@
+package com.battleship.battleship_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/battleship/battleship_game/BattleshipGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battleship.battleship_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
